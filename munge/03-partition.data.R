7c9e2bcf90a14249986f2194ab6dc7e0dacb2f32
irishlouis <louissmith@gmail.com>
2016-08-05 10:41:54 +0000

    confirm row counter, write cached train/test datasets to dropbox

--- munge/03-partition.data.R
+++ munge/03-partition.data.R
@@ -8,8 +8,12 @@ set.seed(789465)
 s <- sample(1:rows, round(0.6*rows))
 setkey(summary.dt, n)
 
-summary.dt[n %in% s][, .N, device_id]
+# check balanced
+summary.dt[,n:=1,][,n:=cumsum(n) ,device_id ]
+summary.dt[n %in% s, .N, device_id]
+
 training.epochs <- summary.dt[n %in% s][,.(device_id, epoch_id),]
+
 training <- summary.dt[n %in% s][,':='(n=NULL,
                                        steps=NULL,
                                        epoch_id=NULL),]
@@ -22,5 +26,5 @@ cache("training")
 cache("testing")
 
 # # write to file 
-# fwrite(training, "training.csv")
-# fwrite(testing, "testing.csv")
+system('cp ~/actigraphy_device_id/cache/testing.RData ~/Dropbox/UCD_MSc_uncompressed/')
+system('cp ~/actigraphy_device_id/cache/training.RData ~/Dropbox/UCD_MSc_uncompressed/')
